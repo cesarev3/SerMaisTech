9b83ee6d42c379bd27def5bfab9848c7b2f97169
cesarev3 <cesardireto@gmail.com>
2023-11-03 22:13:22 -0300

    1) Criação de arquivo executável 2) Criação classe desafio_4d 3) Criação da classe Báskara

--- src/basic.java
+++ src/basic.java
@@ -1,7 +1,8 @@
-import java.util.Scanner;
 
 public class basic {
+
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
     }
 }
---
+++ src/desafio_4d.java
@@ -0,0 +1,6 @@
+public class desafio_4d {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        scanner.next();
+    }
+}
