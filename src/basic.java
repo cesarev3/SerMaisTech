2fe85cca5f72832807a91b9f42e5d60e05370f2e
cesarev3 <cesardireto@gmail.com>
2023-11-10 00:04:08 -0300

    Novos arquivos e dasafio aula 4

--- src/basic.java
+++ src/basic.java
@@ -2,7 +2,9 @@
 public class basic {
 
     public static void main(String[] args) {
-
         System.out.println("Hello World!");
+
+//        System.out.println("Hello, " + args[0] + " " + args[1]);
+//        System.out.println("Hello, " + args[0]);
     }
 }
